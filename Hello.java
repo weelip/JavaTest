8e54da6bbe1c46325aec83d1b2815e2eadf9909a
weelip <67048936+weelip@users.noreply.github.com>
2020-06-17 17:32:26 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-class Demo  {  
-  public static void main(String args[])    {  
+public class Demo  {  
+  public static void main(String[] args)    {  
     System.out.print("Hello!");  
     System.out.print("World!!!");  
   }  
